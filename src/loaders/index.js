fd82e5ec8f6a899044594a7f1478ec6644d14af6
SangHun Kang <myks790@gmail.com>
2020-08-21 11:12:02 +0900

    cors 추가 , dockerignore 추가

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,8 +1,10 @@
+import cors from 'cors';
 import expressLoader from './expressLoader';
 import passportLoader from './passportLoader';
 import sessionLoader from './sessionLoader';
 
 export default (app) => {
+  app.use(cors());
   sessionLoader(app);
   passportLoader(app);
   expressLoader(app);
