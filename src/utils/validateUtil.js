e58d5d9bf6f97a4a11175775b1547d7c573f6cef
SangHun Kang <myks790@gmail.com>
2020-08-21 13:48:13 +0900

    유효성 검사 비활성화

--- src/utils/validateUtil.js
+++ src/utils/validateUtil.js
@@ -9,20 +9,21 @@ const validateUtil = {
   },
 
   isAuth: (req, res, next) => {
-    if (!!req.user && req.user.role === '인증') {
-      next();
-    } else {
-      res.status(401).end();
-    }
+    // if (!!req.user && req.user.role === '인증') {
+    //   next();
+    // } else {
+    //   res.status(401).end();
+    // }
+    next();
   },
 
   isUser: (req, res, next) => {
-    if (!!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
-      next();
-    } else {
-      res.status(401).end();
-    }
-    // next();
+    // if (!!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
+    //   next();
+    // } else {
+    //   res.status(401).end();
+    // }
+    next();
   },
 
   boardAuth: (req, res, next) => {
