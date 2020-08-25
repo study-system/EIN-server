264d07239fa116e334caa656aab5228044542367
SangHun Kang <myks790@gmail.com>
2020-08-25 20:59:35 +0900

    일반 회원이 인증 게시글을 쓸때 예외 처리

--- src/utils/validateUtil.js
+++ src/utils/validateUtil.js
@@ -18,7 +18,10 @@ const validateUtil = {
   },
 
   isUser: (req, res, next) => {
-    if (!!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
+    const {
+      auth,
+    } = req.query;
+    if (auth === 'no' && !!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
       next();
     } else {
       res.status(401).end();
