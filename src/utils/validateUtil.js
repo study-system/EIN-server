5acb75ab875a7eba47254f92c5aa20f81bca9993
SangHun Kang <myks790@gmail.com>
2020-08-25 21:27:12 +0900

    권한 버그 수정

--- src/utils/validateUtil.js
+++ src/utils/validateUtil.js
@@ -18,10 +18,7 @@ const validateUtil = {
   },
 
   isUser: (req, res, next) => {
-    const {
-      auth,
-    } = req.query;
-    if (auth === 'no' && !!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
+    if (!!req.user && (req.user.role === '인증' || req.user.role === '일반' || req.user.role === '관리자')) {
       next();
     } else {
       res.status(401).end();
