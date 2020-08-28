24e2851f6c98448e723f2126be63993cc5fc4f08
SangHun Kang <myks790@gmail.com>
2020-08-28 13:28:07 +0900

    세션 설정 변경

--- src/loaders/sessionLoader.js
+++ src/loaders/sessionLoader.js
@@ -10,7 +10,7 @@ export default (app) => {
   app.use(session({
     store: new RedisStore({ client: redisClient }),
     secret: 'SAME_SECRET',
-    saveUninitialized: true,
-    resave: false,
+    saveUninitialized: false,
+    resave: true,
   }));
 };
